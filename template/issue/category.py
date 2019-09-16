b45a482a84c2e817d0590e6b186e4b020bc0079b
Will <seoktaehyeon@msn.com>
2019-09-16 11:56:03 +0800

    update for svc branch

--- template/issue/category.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-class Category(object):
-    _table_ = ''
--- template/issue/overall.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-class Overall(object):
-    _table_ = 'overall'

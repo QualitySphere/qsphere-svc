e42e53fefff3f4eb84e184e20de856cd85f6a121
Will <seoktaehyeon@msn.com>
2019-10-11 20:23:38 +0800

    fix health check script

--- svc/health_check.sh
+++ svc/health_check.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -ex
-curl -sv http://localhost:6001/api/jira/connectio 2>&1 | grep '^< HTTP' | grep '200 OK'
+curl -sv http://localhost:6001/api/jira/connection 2>&1 | grep '^< HTTP' | grep '200 OK'

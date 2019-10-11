2540b50af45d27af85d7b417e0fa466c9324e26c
Will <seoktaehyeon@msn.com>
2019-10-11 22:11:35 +0800

    update health check for qap-svc

--- svc/health_check.sh
+++ svc/health_check.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -ex
-curl -sv http://localhost:6001/api/jira/connection 2>&1 | grep '^< HTTP' | grep '200 OK'
+python3 -c "import requests; print(requests.get('http://localhost:6001/api/jira/connection').status_code)" | grep 200

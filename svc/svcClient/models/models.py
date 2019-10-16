3c9cf9650f4720c729e9f8b22356c3453f4d570c
Will <seoktaehyeon@msn.com>
2019-10-16 11:17:14 +0800

    update health check function

--- svc/svcClient/models/models.py
+++ svc/svcClient/models/models.py
@@ -2,18 +2,19 @@
 # -*- coding: utf-8 -*-
 
 
-from clientJira.db.db import db
+from svcClient.db.db import db
 from pony.orm import Database, Required, PrimaryKey, Set, StrArray, IntArray, Json, Optional
 import uuid
 from datetime import datetime
 
 
 class Connection(db.Entity):
-    # 连接JIRA的地址和帐密信息
+    # 连接缺陷/用例管理工具的地址和帐密信息
     _table_ = 'connection'
 
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
-    type = Required(str, default='jira')
+    type = Required(str, default='issue')  # issue/case
+    server_type = Required(str, default='jira')  # jira/zentao/testlink/bugzilla
     server = Required(str)
     account = Required(str)
     password = Required(str)
@@ -43,9 +44,9 @@ class Sprint(db.Entity):
     features = Required(StrArray)  # 功能标签列表: 功能1, 功能2, ...
     rcs = Required(StrArray)  # RC标签列表: rc1,rc2,rc3,rc4,rc5, ...
     issue_found_since = Required(StrArray, default=['regression_improve', 'qa_missed', 'new_feature', 'customer'])  # 问题发现来源标签列表
-    issue_status = Required(Json)  # 问题状态: fixing: '待办, 进行中, 新发现', fixed: '开发完成', verified: '已关闭'
+    issue_status = Required(Json)  # 问题状态: fixing: '', fixed: '', verified: ''
     issue_categories = Required(StrArray, default=['regression', 'previous', 'newfeature', 'others'])  # 问题类别标签列表
-    queries = Optional(Json)  # jql 集
+    queries = Optional(Json)  # 查询语句集: jql/././.
     active = Required(str, default='enable')  # 该迭代的激活状态: enable, disable, delete
     issue_project_latest = Set('IssueProjectLatest')
     issue_customer_latest = Set('IssueCustomerLatest')
@@ -62,8 +63,8 @@ class IssueProjectLatest(db.Entity):
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     capture_at = Required(datetime)
     sprint = Required(Sprint)
-    categories = Required(Json)  # 缺陷类别
-    found_since = Required(Json)  # 缺陷发现来源: new_feature, regression_improve, qa_missed
+    categories = Required(Json)  # 缺陷类别:
+    found_since = Required(Json)  # 缺陷发现来源: new_feature: '', regression_improve: '', qa_missed: ''
 
 
 class IssueCustomerLatest(db.Entity):
@@ -84,9 +85,9 @@ class IssueSprint(db.Entity):
     capture_at = Required(datetime)
     sprint = Required(Sprint)
     status = Required(Json)  # 缺陷状态: total, fixing, fixed, verified
-    categories = Required(Json)  # 缺陷类别: regression, previous, code_change, others
-    found_since = Required(Json)  # 缺陷发现来源: new_feature, regression_improve, qa_missed
-    found_in_rcs = Required(Json)  # 缺陷发现版本: rc1, rc2, rc3, rc4, rc5
+    categories = Required(Json)  # 缺陷类别: regression: '', previous: '', new_feature: '', others: ''
+    found_since = Required(Json)  # 缺陷发现来源: new_feature: '', regression_improve: '', qa_missed: ''
+    found_in_rcs = Required(Json)  # 缺陷发现版本: rc1: '', rc2: '', rc3: '', . . .
 
 
 class IssueSprintLatest(db.Entity):
@@ -120,5 +121,5 @@ class IssueFeatureLatest(db.Entity):
     capture_at = Required(datetime)
     sprint = Required(Sprint)
     name = Required(str)  # 功能名称
-    status = Required(Json)  # 缺陷状态: total, fixing, fixed, verified
+    status = Required(Json)  # 缺陷状态: total: '', fixing: '', fixed: '', verified: ''
 

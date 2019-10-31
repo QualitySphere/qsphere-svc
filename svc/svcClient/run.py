e32e85ecf41b8da44a283b5feec62a490beda4cc
Will <seoktaehyeon@msn.com>
2019-10-31 17:04:17 +0800

    add machine check function and api

--- svc/svcClient/run.py
+++ svc/svcClient/run.py
@@ -5,7 +5,9 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import connexion
 from db.db import db
-
+import os
+import yaml
+import json
 import logging
 from pony.orm import set_sql_debug
 from apscheduler.schedulers.background import BackgroundScheduler as APScheduler
@@ -18,6 +20,30 @@ logging.basicConfig(level=logging.INFO, format='[ %(asctime)s ] %(levelname)s %(
 def svc_job():
     # 调用同步接口
     requests.get(url='http://127.0.0.1:6001/api/issue/sync', timeout=30)
+
+
+def machine_check_job():
+    vms = list()
+    # Load machine list from YAML file
+    machine_list_file = os.path.join('svcClient', 'specs', 'machine-list.yaml')
+    machine_status_file = os.path.join('svcClient', 'specs', 'machine-status.json')
+    with open(machine_list_file, 'r') as f:
+        vms_list = yaml.full_load(f.read())
+    # Check machine in machine list
+    for vm_name, vm_ip in vms_list.items():
+        vm = {
+            'name': vm_name,
+            'ip': vm_ip,
+            'status': 'OFFLINE'
+        }
+        output = os.system('ping -c 1 -W 1 ' + vm_ip)
+        if output == 0:
+            vm['status'] = 'ONLINE'
+        vms.append(vm)
+    # Generate machine status JSON file
+    with open(machine_status_file, 'w') as f:
+        f.write(json.dumps(vms))
+    return True
 
 
 if __name__ == '__main__':
@@ -40,6 +66,7 @@ if __name__ == '__main__':
     # 创建定时任务, 每小时执行一次同步
     scheduler = APScheduler()
     scheduler.add_job(func=svc_job, id='svc_job', trigger='interval', hours=1, replace_existing=True)
+    scheduler.add_job(func=machine_check_job, id='machine_check_job', trigger='interval', seconds=600, replace_existing=True)
     scheduler.start()
 
     app.run(port=6001, debug=True)

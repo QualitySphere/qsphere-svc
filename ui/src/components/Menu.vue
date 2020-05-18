8c80342d3430e7698768d900500a9f74f8061325
Will <seoktaehyeon@msn.com>
2020-05-18 18:33:54 +0800

    complete iframe for grafana

--- ui/src/components/Menu.vue
+++ ui/src/components/Menu.vue
@@ -1,20 +1,28 @@
 <template>
   <el-menu
     :router="true"
-    :default-active="1"
+    default-active="1-1"
     background-color="#EDEDED"
     text-color="#909399"
     active-text-color="#212529">
     <el-menu-item-group title="Dashboard" style="margin-top: 15px;">
-    <el-menu-item index="1" route="/overview">
-      <span style="margin-left: 15px;">Overview</span>
-    </el-menu-item>
-    <el-menu-item index="2" route="/project">
-      <span style="margin-left: 15px;">Project</span>
-    </el-menu-item>
-    <el-menu-item index="3" route="/sprint">
-      <span style="margin-left: 15px;">Sprint</span>
-    </el-menu-item>
+      <el-menu-item index="1-1" route="/overview">
+        <span style="margin-left: 15px;">Overview</span>
+      </el-menu-item>
+      <el-menu-item index="1-2" route="/project">
+        <span style="margin-left: 15px;">Project</span>
+      </el-menu-item>
+      <el-menu-item index="1-3" route="/sprint">
+        <span style="margin-left: 15px;">Sprint</span>
+      </el-menu-item>
+    </el-menu-item-group>
+    <el-menu-item-group title="Portal" style="margin-top: 15px;">
+      <el-menu-item index="2-1" route="/">
+        <span style="margin-left: 15px;">JIRA</span>
+      </el-menu-item>
+      <el-menu-item index="2-2" route="/">
+        <span style="margin-left: 15px;">TestLink</span>
+      </el-menu-item>
     </el-menu-item-group>
   </el-menu>
 </template>
--- ui/src/views/Dashboard.vue
+++ ui/src/views/Dashboard.vue
@@ -1,18 +1,121 @@
 <template>
-  <div class="dashboard">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Dashboard"/>
+  <div style="width: 100%; height: 100%;">
+    <el-row type="flex" style="height: 5%;">
+    <el-form
+      :inline="true"
+      size="small"
+      label-position="left">
+      <el-form-item
+        label="Tracker">
+        <el-select
+          @focus="listTracker"
+          v-model="trackers.connection_name"
+          placeholder="Select Tracker">
+          <el-option
+            v-for="item in trackers"
+            :key="item.connection_id"
+            :label="item.connection_name"
+            :value="item.connection_name">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item
+        v-if="$route.path == '/project' || $route.path == '/sprint'"
+        label="Project"
+        style="margin-left: 20px;">
+        <el-select
+          @focus="listProject"
+          v-model="projects.project_name"
+          @change="updateUrlForProject"
+          placeholder="Select Project">
+          <el-option
+            v-for="item in projects"
+            :key="item.project_id"
+            :label="item.project_name"
+            :value="item.project_name">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item
+        v-if="$route.path == '/sprint'"
+        label="Sprint"
+        style="margin-left: 20px;">
+        <el-select
+          @focus="listSprint"
+          v-model="sprints.sprint_name"
+          @change="updateUrlForSprint"
+          placeholder="Select Sprint">
+          <el-option
+            v-for="item in sprints"
+            :key="item.sprint_id"
+            :label="item.sprint_name"
+            :value="item.sprint_name">
+          </el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+    </el-row>
+    <iframe
+      frameborder="0"
+      sandbox="allow-scripts allow-same-origin"
+      :src="url"
+      style="width: 100%; height: 95%;">
+    </iframe>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      baseUrl: '/dashboard/d/qap/quality-assurance-platform?orgId=1&theme=light&kiosk&refresh=1m',
+      url: '/dashboard/d/qap/quality-assurance-platform?orgId=1&theme=light&kiosk&refresh=1m',
+      trackers: [],
+      projects: [],
+      sprints: [],
+      startDate: '2020-04-30',
+      endDate: '2020-05-13'
+    }
+  },
+  methods: {
+    updateUrlForProject () {
+      this.sprints.sprint_name = ''
+      this.url = this.baseUrl + '&var-PROJECT=' + this.projects.project_name + '&var-START_DATE=' + this.startDate + '&var-END_DATE=' + this.endDate
+    },
+    updateUrlForSprint () {
+      this.url = this.baseUrl + '&var-PROJECT=' + this.projects.project_name + '&var-SPRINT=' + this.sprints.sprint_name + '&var-START_DATE=' + this.startDate + '&var-END_DATE=' + this.endDate
+    },
+    listTracker () {
+      axios.get('/api/connection')
+        .then((response) => {
+          console.log(response)
+          this.trackers = response.data.detail.results
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    listProject () {
+      axios.get('/api/project')
+        .then((response) => {
+          console.log(response)
+          this.projects = response.data.detail.results
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    listSprint () {
+      axios.get('/api/sprint')
+        .then((response) => {
+          console.log(response)
+          this.sprints = response.data.detail.results
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
   }
 }
 </script>

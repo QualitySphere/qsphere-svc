024fd084664427f3e4aaa8b569a1d47e7ec1c6bf
Will <seoktaehyeon@msn.com>
2020-05-20 09:41:32 +0800

    rewrite for tracker function

--- svc/svcClient/db/db.py
+++ svc/svcClient/db/db.py
@@ -8,9 +8,9 @@ import os
 
 db = Database(
     provider='postgres',
-    user=os.getenv('PG_USER') if os.getenv('PG_USER') else 'postgres',
-    password=os.getenv('PG_PASSWORD') if os.getenv('PG_PORT') else 'password',
-    host=os.getenv('PG_SERVER') if os.getenv('PG_SERVER') else '127.0.0.1',
-    port=os.getenv('PG_PORT') if os.getenv('PG_PORT') else '5432',
-    database='qap',
+    user=os.getenv('PG_USER') or 'postgres',
+    password=os.getenv('PG_PASSWORD') or 'password',
+    host=os.getenv('PG_SERVER') or '127.0.0.1',
+    port=os.getenv('PG_PORT') or '5432',
+    database='qsphere'
 )

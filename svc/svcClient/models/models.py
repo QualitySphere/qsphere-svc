a95bbed4bc6aef4b1e30d02f530ad71d3f122143
Will <seoktaehyeon@msn.com>
2020-05-20 19:00:26 +0800

    complete to rewrite sprint

--- svc/svcClient/models/models.py
+++ svc/svcClient/models/models.py
@@ -11,7 +11,6 @@ from datetime import datetime
 class Tracker(db.Entity):
     # Tracker Information
     _table_ = 'tracker'
-
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     name = Required(str)
     type = Required(str)
@@ -23,7 +22,6 @@ class Tracker(db.Entity):
 class Project(db.Entity):
     # Project Information
     _table_ = 'project'
-
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     name = Required(str)
     tracker = Optional(Json)    # {'issue': {'id': 'UUID'}, 'case': {'id': 'UUID'}}
@@ -35,19 +33,16 @@ class Project(db.Entity):
 class Sprint(db.Entity):
     # Sprint Information
     _table_ = 'sprint'
-
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
-    name = Required(str)  # 名称
+    name = Required(str)
     project = Required(Project)
-    version = Required(str)  # 迭代版本
-    issue_types = Required(StrArray)  # 问题筛选类型: Improvement, 缺陷
-    requirements = Required(StrArray)  # 需求标签列表: 功能1, 功能2, ...
-    rcs = Required(StrArray)  # RC标签列表: rc1,rc2,rc3,rc4,rc5, ...
-    issue_found_since = Required(StrArray, default=['RegressionImprove', 'QAMissed', 'NewFeature', 'Customer'])  # 问题发现来源标签列表
-    issue_status = Required(Json)  # 问题状态: fixing: '', fixed: '', verified: ''
-    issue_categories = Required(StrArray, default=['Regression', 'Previous', 'NewFeature', 'Others'])  # 问题类别标签列表
-    queries = Optional(Json)  # 查询语句集: jql/././.
-    status = Required(str, default='active')  # 该迭代的激活状态: enable, disable, delete
+    version = Required(str)  # sprint version tag
+    requirements = Required(StrArray)   # list(): req1, req2
+    rcs = Required(StrArray)  # RC tags list: RC1, RC2
+    issue = Optional(Json)   # dict(): keys: types, found_since, statuses, categories
+    case = Optional(Json)
+    queries = Optional(Json)  # dict(): keys: case, issue
+    status = Required(str, default='active')  # active, disable, delete
     issue_project_latest = Set('IssueProjectLatest')
     issue_customer_latest = Set('IssueCustomerLatest')
     issue_sprint_latest = Set('IssueSprintLatest')
@@ -59,7 +54,6 @@ class Sprint(db.Entity):
 class IssueProjectLatest(db.Entity):
     # 项目维度 最新缺陷数据
     _table_ = 'issue_project_latest'
-
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     capture_at = Required(datetime)
     sprint = Required(Sprint)
@@ -70,7 +64,6 @@ class IssueProjectLatest(db.Entity):
 class IssueCustomerLatest(db.Entity):
     # 项目维度 最新客户反馈缺陷数量
     _table_ = 'issue_customer_latest'
-
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     capture_at = Required(datetime)
     sprint = Required(Sprint)
@@ -80,7 +73,6 @@ class IssueCustomerLatest(db.Entity):
 class IssueSprint(db.Entity):
     # 迭代版本维度 缺陷数据
     _table_ = 'issue_sprint'
-
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     capture_at = Required(datetime)
     sprint = Required(Sprint)
@@ -93,7 +85,6 @@ class IssueSprint(db.Entity):
 class IssueSprintLatest(db.Entity):
     # 迭代版本维度 最新缺陷数据
     _table_ = 'issue_sprint_latest'
-
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     capture_at = Required(datetime)
     sprint = Required(Sprint)
@@ -104,7 +95,6 @@ class IssueSprintLatest(db.Entity):
 class IssueFeature(db.Entity):
     # 迭代版本功能维度 缺陷数据
     _table_ = 'issue_feature'
-
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     capture_at = Required(datetime)
     sprint = Required(Sprint)
@@ -116,7 +106,6 @@ class IssueFeature(db.Entity):
 class IssueFeatureLatest(db.Entity):
     # 迭代版本功能维度 最新缺陷数据
     _table_ = 'issue_feature_latest'
-
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     capture_at = Required(datetime)
     sprint = Required(Sprint)

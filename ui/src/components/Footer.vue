843d2e5678d20201594863b93982f3de5b99f7d1
Will <seoktaehyeon@msn.com>
2020-06-04 23:26:48 +0800

    update footer

--- ui/src/components/Footer.vue
+++ ui/src/components/Footer.vue
@@ -1,13 +1,7 @@
 <template>
-  <el-link type="info" disabled style="margin: 15px;">{{ footer }}</el-link>
+  <div>
+    <el-link href="https://QualitySphere.github.io" target="_blank" type="info" style="margin: 15px;">Homepage</el-link>
+    |
+    <el-link href="https://github.com/QualitySphere/qsphere" target="_blank" type="info" style="margin: 15px;">Code in GitHub</el-link>
+  </div>
 </template>
-
-<script>
-export default {
-  data () {
-    return {
-      footer: 'Powered By Will'
-    }
-  }
-}
-</script>

cdf68688615faa9fa3bcf97e254c06c2973de940
seoktaehyeon <seoktaehyeon@msn.com>
2021-01-23 23:58:02 +0800

    complete rewrite swagger

--- svc/svcClient/api/apiHealth.py
+++ svc/svcClient/api/apiHealth.py
@@ -12,9 +12,9 @@ def health_check():
     }, 200
 
 
-def machine_check():
-    machine_status_file = os.path.join('svcClient', 'specs', 'machine-status.json')
-    with open(machine_status_file, 'r') as f:
+def host_check():
+    host_status_file = os.path.join('svcClient', 'specs', 'machine-status.json')
+    with open(host_status_file, 'r') as f:
         results = f.read()
     return json.loads(results), 200
 

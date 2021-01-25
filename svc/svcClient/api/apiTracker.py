d016799410c832bc202be87930b8b91a10678757
seoktaehyeon <seoktaehyeon@msn.com>
2021-01-25 19:12:13 +0800

    update 1.1 for tracker

--- svc/svcClient/api/apiTracker.py
+++ svc/svcClient/api/apiTracker.py
@@ -14,36 +14,30 @@ def get_tracker(tracker_id: str):
     try:
         tracker = svcTracker.get_tracker(tracker_id)
         return {
-            'title': 'Get Tracker Succeed',
+            'title': 'Succeed to get Tracker',
             'detail': tracker
         }, 200
     except Exception as e:
-        raise DefaultError(title='Get Tracker Succeed', detail=str(e))
+        raise DefaultError(title='Failed to get Tracker', detail=str(e))
 
 
 def add_tracker(body):
     """
-    POST /api/tracker
+    POST /api/trackers
     :param body:
     :return: tracker_id
     """
-    _name = body.get('name')
-    _type = body.get('type')
-    _info = body.get('info')
-    _secret = body.get('secret')
     try:
-        tracker_id = svcTracker.add_tracker(tracker_name=_name,
-                                            tracker_type=_type,
-                                            tracker_info=_info,
-                                            tracker_secret=_secret)
+        tracker_id = svcTracker.add_tracker(tracker_name=body.get('name'),
+                                            tracker_type=body.get('type'),
+                                            tracker_info=body.get('info'),
+                                            tracker_secret=body.get('token'))
         return {
-            'title': 'Add Tracker Succeed',
-            'detail': {
-                'id': tracker_id
-            }
+            'title': 'Succeed to add Tracker',
+            'detail': tracker_id
         }, 200
     except Exception as e:
-        raise DefaultError(title='Create Tracker Failed', detail=str(e))
+        raise DefaultError(title='Failed to add Tracker', detail=str(e))
 
 
 def update_tracker(tracker_id, body):
@@ -53,46 +47,54 @@ def update_tracker(tracker_id, body):
     :param body:
     :return: tracker_id
     """
-    _name = body.get('name')
-    _type = body.get('type')
-    _info = body.get('info')
-    _secret = body.get('secret')
     try:
         tracker_id = svcTracker.update_tracker(tracker_id=tracker_id,
-                                               tracker_name=_name,
-                                               tracker_type=_type,
-                                               tracker_info=_info,
-                                               tracker_secret=_secret)
+                                               tracker_name=body.get('name'),
+                                               tracker_type=body.get('type'),
+                                               tracker_info=body.get('info'),
+                                               tracker_secret=body.get('token'))
         return {
-            'title': 'Update Tracker Succeed',
-            'detail': {
-                'id': tracker_id
-            }
+            'title': 'Succeed to update Tracker',
+            'detail': tracker_id
         }, 200
     except Exception as e:
-        raise DefaultError(title='Update Connection Failed', detail=str(e))
+        raise DefaultError(title='Failed to update Tracker', detail=str(e))
 
 
-def update_tracker_status():
+def update_tracker_status(tracker_id, status):
+    """
+    PUT /api/tracker/{tracker_id}/status
+    :param tracker_id:
+    :param status:
+    :return:
+    """
+    try:
+        tracker_status = svcTracker.set_tracker_status(tracker_id=tracker_id, tracker_status=status)
+        return {
+            'title': 'Succeed to change Tracker status',
+            'detail': tracker_status
+        }, 200
+    except Exception as e:
+        raise DefaultError(title='Failed to change Tracker status', detail=str(e))
     pass
 
 
 def list_tracker():
     """
-    GET /api/tracker
+    GET /api/trackers
     :return: [{}]
     """
     try:
         trackers = svcTracker.list_tracker(tracker_status=['active', 'disable'])
         return {
-            'title': 'List Tracker Succeed',
+            'title': 'Succeed to list Tracker',
             'detail': {
                 'count': len(trackers),
                 'results': trackers
             }
         }, 200
     except Exception as e:
-        raise DefaultError(title='List Tracker Failed', detail=str(e))
+        raise DefaultError(title='Failed to list Tracker', detail=str(e))
 
 
 def delete_tracker(tracker_id):
@@ -104,40 +106,40 @@ def delete_tracker(tracker_id):
     try:
         svcTracker.set_tracker_status(tracker_id, 'delete')
         return {
-            'title': 'Delete Tracker Succeed'
+            'title': 'Succeed to delete Tracker'
         }, 204
     except Exception as e:
-        raise DefaultError(title='Delete Tracker Failed', detail=str(e))
+        raise DefaultError(title='Failed to delete Tracker', detail=str(e))
 
 
-def active_tracker(tracker_id):
-    """
-    PUT /api/tracker/{tracker_id}/active
-    :param tracker_id:
-    :return:
-    """
-    try:
-        svcTracker.set_tracker_status(tracker_id, 'active')
-        return {
-            'title': 'Active Tracker Succeed'
-        }, 200
-    except Exception as e:
-        raise DefaultError(title='Active Tracker Failed', detail=str(e))
+# def active_tracker(tracker_id):
+#     """
+#     PUT /api/tracker/{tracker_id}/active
+#     :param tracker_id:
+#     :return:
+#     """
+#     try:
+#         svcTracker.set_tracker_status(tracker_id, 'active')
+#         return {
+#             'title': 'Active Tracker Succeed'
+#         }, 200
+#     except Exception as e:
+#         raise DefaultError(title='Active Tracker Failed', detail=str(e))
 
 
-def disable_tracker(tracker_id):
-    """
-    PUT /api/tracker/{tracker_id}/disable
-    :param tracker_id:
-    :return:
-    """
-    try:
-        svcTracker.set_tracker_status(tracker_id, 'disable')
-        return {
-            'title': 'Disable Tracker Succeed'
-        }, 200
-    except Exception as e:
-        raise DefaultError(title='Disable Tracker Failed', detail=str(e))
+# def disable_tracker(tracker_id):
+#     """
+#     PUT /api/tracker/{tracker_id}/disable
+#     :param tracker_id:
+#     :return:
+#     """
+#     try:
+#         svcTracker.set_tracker_status(tracker_id, 'disable')
+#         return {
+#             'title': 'Disable Tracker Succeed'
+#         }, 200
+#     except Exception as e:
+#         raise DefaultError(title='Disable Tracker Failed', detail=str(e))
 
 
 def list_tracker_project(tracker_id):
@@ -149,52 +151,52 @@ def list_tracker_project(tracker_id):
     try:
         projects = svcTracker.list_tracker_project(tracker_id)
         return {
-                   'title': 'List Project From Tracker Succeed',
-                   'detail': {
-                       'count': len(projects),
-                       'results': projects
-                   }
-               }, 200
+           'title': 'Succeed to list Project from Tracker',
+           'detail': {
+               'count': len(projects),
+               'results': projects
+           }
+       }, 200
     except Exception as e:
-        raise DefaultError(title='List Project From Tracker Failed', detail=str(e))
+        raise DefaultError(title='Failed to list Project from Tracker', detail=str(e))
 
 
-def list_tracker_issue_type(tracker_id):
-    """
-    GET /api/tracker/{tracker_id}/issue_types
-    :param tracker_id:
-    :return: [{'key': '', 'value': ''}]
-    """
-    try:
-        types = svcTracker.list_tracker_issue_type(tracker_id)
-        return {
-                   'title': 'List Issue Types From Tracker Succeed',
-                   'detail': {
-                       'count': len(types),
-                       'results': types
-                   }
-               }, 200
-    except Exception as e:
-        raise DefaultError(title='List Issue Types From Tracker Failed', detail=str(e))
-
-
-def list_tracker_issue_status(tracker_id):
-    """
-    GET /api/tracker/{tracker_id}/issue_statuses
-    :param tracker_id:
-    :return: [{'key': '', 'value': ''}]
-    """
-    try:
-        statuses = svcTracker.list_tracker_issue_status(tracker_id)
-        return {
-                   'title': 'List Issue Statuses From Tracker Succeed',
-                   'detail': {
-                       'count': len(statuses),
-                       'results': statuses
-                   }
-            }, 200
-    except Exception as e:
-        raise DefaultError(title='List Issue Statuses From Tracker Failed', detail=str(e))
+# def list_tracker_issue_type(tracker_id):
+#     """
+#     GET /api/tracker/{tracker_id}/issue_types
+#     :param tracker_id:
+#     :return: [{'key': '', 'value': ''}]
+#     """
+#     try:
+#         types = svcTracker.list_tracker_issue_type(tracker_id)
+#         return {
+#                    'title': 'List Issue Types From Tracker Succeed',
+#                    'detail': {
+#                        'count': len(types),
+#                        'results': types
+#                    }
+#                }, 200
+#     except Exception as e:
+#         raise DefaultError(title='List Issue Types From Tracker Failed', detail=str(e))
+#
+#
+# def list_tracker_issue_status(tracker_id):
+#     """
+#     GET /api/tracker/{tracker_id}/issue_statuses
+#     :param tracker_id:
+#     :return: [{'key': '', 'value': ''}]
+#     """
+#     try:
+#         statuses = svcTracker.list_tracker_issue_status(tracker_id)
+#         return {
+#                    'title': 'List Issue Statuses From Tracker Succeed',
+#                    'detail': {
+#                        'count': len(statuses),
+#                        'results': statuses
+#                    }
+#             }, 200
+#     except Exception as e:
+#         raise DefaultError(title='List Issue Statuses From Tracker Failed', detail=str(e))
 
 
 def list_tracker_sprint(tracker_id):
@@ -244,7 +246,11 @@ def list_tracker_issue_field_value(tracker_id, issue_field_id, project_id):
     :return: [{'key': '', 'value': ''}]
     """
     try:
-        values = svcTracker.list_tracker_issue_field_value(tracker_id, issue_field_id, project_id)
+        values = svcTracker.list_tracker_issue_field_value(
+            tracker_id=tracker_id,
+            project_id=project_id,
+            field_key=issue_field_id,
+        )
         return {
             'title': 'Succeed to list Issue Field Value from tracker',
             'detail': {

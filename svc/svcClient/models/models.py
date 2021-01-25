2603ecdba0c93fb70742a63199e0319a0fb350f4
seoktaehyeon <seoktaehyeon@msn.com>
2021-01-25 15:51:56 +0800

    rewrite svcTracker

--- svc/svcClient/models/models.py
+++ svc/svcClient/models/models.py
@@ -9,40 +9,91 @@ from datetime import datetime
 
 
 class Tracker(db.Entity):
-    # Tracker Information
+    """
+    Tracker Information
+    """
     _table_ = 'tracker'
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     name = Required(str)
+    # Type: jira
     type = Required(str)
-    info = Optional(Json)  # JIRA: {host: '', account: ''}
-    secret = Optional(str)  # JIRA: 'password'
-    status = Required(str, default='active')  # active, disable, delete
+    # jira: {'host': 'string', 'account': 'string'}
+    info = Optional(Json)
+    # jira: password
+    token = Optional(str)
+    # Status: active, disable, delete
+    status = Required(str, default='active')
 
 
 class Project(db.Entity):
-    # Project Information
+    """
+    Project Information
+    """
     _table_ = 'project'
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     name = Required(str)
-    tracker = Optional(Json)    # {'issue': {'id': 'UUID'}, 'case': {'id': 'UUID'}}
-    project = Optional(Json)    # {'issue': {'key': ''}, 'case': {'key': ''}}
-    status = Required(str, default='active')  # active, disable, delete
+    # Issue Tracker: {'tracker_id': 'string', 'project_key': 'string', 'project_value': 'string'}
+    issue_tracker = Optional(Json)
+    # Case Tracker: {'tracker_id': 'string', 'project_key': 'string', 'project_value': 'string'}
+    case_tracker = Optional(Json)
+    # Status: active, disable, delete
+    status = Required(str, default='active')
+    sprints = Set('Sprint')
+
+
+class IssueConfig(db.Entity):
+    """
+    Sprint Issue Configuration
+    """
+    _table_ = 'issue_config'
+    uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
+    # Issue Found in Sprint: {'field': 'string', 'value': ['string']}
+    issue_sprint = Required(Json)
+    # Issue Found in Requirement: {'field': 'string', 'value': ['string']}
+    issue_requirement = Required(Json)
+    # Issue Found in Version: {'field': 'string', 'value': ['string']}
+    issue_version = Required(Json)
+    # Issue Found in RC: {'field': 'string', 'value': ['string']}
+    issue_rc = Required(Json)
+    # Issue Type: {'field': 'string', 'value': ['string']}
+    issue_type = Required(Json)
+    # Issue Found Since:
+    # {'field': 'string',
+    # 'newfeature': ['string'], 'improve': ['string'],
+    # 'customer': ['string'], 'qamissed': ['string']}
+    issue_since = Required(Json)
+    # Issue Category:
+    # {'field': 'string',
+    # 'newfeature': ['string'], 'regression': ['string'], 'previous': ['string']}
+    issue_category = Required(Json)
+    # Issue Status:
+    # {'field': 'string',
+    # 'fixing': ['string'], 'fixed': ['string'], 'verified': ['string']}
+    issue_status = Required(Json)
+    sprints = Set('Sprint')
+
+
+class CaseConfig(db.Entity):
+    """
+    Sprint Case Configuration
+    """
+    _table_ = 'case_config'
+    uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     sprints = Set('Sprint')
 
 
 class Sprint(db.Entity):
-    # Sprint Information
+    """
+    Sprint Information
+    """
     _table_ = 'sprint'
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     name = Required(str)
     project = Required(Project)
-    version = Required(str)  # sprint version tag
-    requirements = Required(StrArray)   # list(): req1, req2
-    rcs = Required(StrArray)  # RC tags list: RC1, RC2
-    issue = Optional(Json)   # dict(): keys: types, found_since, statuses, categories
-    case = Optional(Json)
-    queries = Optional(Json)  # dict(): keys: case, issue
-    status = Required(str, default='active')  # active, disable, delete
+    issue_config = Optional(IssueConfig)
+    case_config = Optional(CaseConfig)
+    # Status: active, disable, delete
+    status = Required(str, default='active')
     issue_project_latest = Set('IssueProjectLatest')
     issue_customer_latest = Set('IssueCustomerLatest')
     issue_sprint_latest = Set('IssueSprintLatest')
@@ -51,64 +102,107 @@ class Sprint(db.Entity):
     issue_req = Set('IssueReq')
 
 
-class IssueProjectLatest(db.Entity):
-    # 项目维度 最新缺陷数据
-    _table_ = 'issue_project_latest'
+class IssueCaptureSprintLevel(db.Entity):
+    """
+    Capture for Sprint Level Issue Data
+    # _table_ = 'issue_sprint'
+    """
+    _table_ = 'issue_capture_sprint_level'
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     capture_time = Required(datetime)
     sprint = Required(Sprint)
-    categories = Required(Json)  # 缺陷类别:
-    found_since = Required(Json)  # 缺陷发现来源: new_feature: '', regression_improve: '', qa_missed: ''
+    # Issue Status: {'total': int, 'fixing': int, 'fixed': int, 'verified': int}
+    status = Required(Json)
+    # Issue Category: {'newfeature': int, 'regression': int, 'previous': int, 'others': int}
+    category = Required(Json)
+    # Issue Found Since: {'newfeature': int, 'improve': int, 'qamissed': int}
+    since = Required(Json)
+    # Issue Found in RC: {'rc1': int, 'rc2': int, 'rc3': int, ...}
+    rc = Required(Json)
 
 
-class IssueCustomerLatest(db.Entity):
-    # 项目维度 最新客户反馈缺陷数量
-    _table_ = 'issue_customer_latest'
+class IssueCaptureReqLevel(db.Entity):
+    """
+    Capture for Sprint Requirement Level Issue Data
+    # _table_ = 'issue_req'
+    """
+    _table_ = 'issue_capture_req_level'
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     capture_time = Required(datetime)
     sprint = Required(Sprint)
-    count = Required(int)  # 客户反馈的缺陷数量, 缺陷发现来源: customer
+    # Requirement Name
+    name = Required(str)
+    # Issue Status: {'total': int, 'fixing': int, 'fixed': int, 'verified': int}
+    status = Required(Json)
+    # Issue Found in RC: {'rc1': int, 'rc2': int, 'rc3': int, ...}
+    rc = Optional(Json)
 
 
-class IssueSprint(db.Entity):
-    # 迭代版本维度 缺陷数据
-    _table_ = 'issue_sprint'
+class IssueCaptureLatest(db.Entity):
+    """
+    Capture for Latest Issue Data
+    """
+    _table_ = 'issue_capture_latest'
     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
     capture_time = Required(datetime)
     sprint = Required(Sprint)
-    status = Required(Json)  # 缺陷状态: total, fixing, fixed, verified
-    categories = Required(Json)  # 缺陷类别: regression: '', previous: '', new_feature: '', others: ''
-    found_since = Required(Json)  # 缺陷发现来源: new_feature: '', regression_improve: '', qa_missed: ''
-    found_in_rcs = Required(Json)  # 缺陷发现版本: rc1: '', rc2: '', rc3: '', . . .
+    # Capture for project level: {
+    # 'category': {'newfeature': int, 'regression': int, 'previous': int, 'others': int},
+    # 'since': {'newfeature': int, 'improve': int, 'customer': int, 'qamissed': int}
+    # }
+    capture_issue_project = Optional(Json)
+    # Capture for customer level: {
+    # 'customer': int
+    # }
+    capture_issue_customer = Optional(Json)
+    # Capture for sprint level: {
+    # 'rc1': int, 'rc2': int, ...
+    # }
+    capture_issue_sprint = Optional(Json)
+    # Capture for requirement level: {
+    # 'req1': {'total': int, 'fixing': int, 'fixed': int, 'verified': int},
+    # 'req2': {'total': int, 'fixing': int, 'fixed': int, 'verified': int},
+    # }
+    capture_issue_req = Optional(Json)
 
-
-class IssueSprintLatest(db.Entity):
-    # 迭代版本维度 最新缺陷数据
-    _table_ = 'issue_sprint_latest'
-    uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
-    capture_time = Required(datetime)
-    sprint = Required(Sprint)
-    rc = Required(str)  # 迭代版本 RC
-    count = Required(int)  # 缺陷数量
-
-
-class IssueReq(db.Entity):
-    # 迭代版本需求维度 缺陷数据
-    _table_ = 'issue_req'
-    uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
-    capture_time = Required(datetime)
-    sprint = Required(Sprint)
-    name = Required(str)  # 需求名称
-    status = Required(Json)  # 缺陷状态: total, fixing, fixed, verified
-    # found_in_rcs = Required(Json)  # rc1, rc2, rc3, rc4, rc5
-
-
-class IssueReqLatest(db.Entity):
-    # 迭代版本需求维度 最新缺陷数据
-    _table_ = 'issue_req_latest'
-    uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
-    capture_time = Required(datetime)
-    sprint = Required(Sprint)
-    name = Required(str)  # 需求名称
-    status = Required(Json)  # 缺陷状态: total: '', fixing: '', fixed: '', verified: ''
-
+#
+# class IssueProjectLatest(db.Entity):
+#     """
+#     项目维度 最新缺陷数据
+#     """
+#     _table_ = 'issue_project_latest'
+#     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
+#     capture_time = Required(datetime)
+#     sprint = Required(Sprint)
+#     categories = Required(Json)  # 缺陷类别:
+#     found_since = Required(Json)  # 缺陷发现来源: new_feature: '', regression_improve: '', qa_missed: ''
+#
+#
+# class IssueCustomerLatest(db.Entity):
+#     # 项目维度 最新客户反馈缺陷数量
+#     _table_ = 'issue_customer_latest'
+#     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
+#     capture_time = Required(datetime)
+#     sprint = Required(Sprint)
+#     count = Required(int)  # 客户反馈的缺陷数量, 缺陷发现来源: customer
+#
+#
+# class IssueSprintLatest(db.Entity):
+#     # 迭代版本维度 最新缺陷数据
+#     _table_ = 'issue_sprint_latest'
+#     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
+#     capture_time = Required(datetime)
+#     sprint = Required(Sprint)
+#     rc = Required(str)  # 迭代版本 RC
+#     count = Required(int)  # 缺陷数量
+#
+#
+# class IssueReqLatest(db.Entity):
+#     # 迭代版本需求维度 最新缺陷数据
+#     _table_ = 'issue_req_latest'
+#     uuid = PrimaryKey(uuid.UUID, default=uuid.uuid4)
+#     capture_time = Required(datetime)
+#     sprint = Required(Sprint)
+#     name = Required(str)  # 需求名称
+#     status = Required(Json)  # 缺陷状态: total: '', fixing: '', fixed: '', verified: ''
+#
--- svc/svcClient/services/svcTracker.py
+++ svc/svcClient/services/svcTracker.py
@@ -11,25 +11,30 @@
 
 
 from pony.orm import db_session, get, select
-from models.models import Tracker
+from models.models import Tracker, Project
 from utils.jiraClient import JiraSession
 
 
 @db_session
-def add_tracker(tracker_name: str, tracker_type: str, tracker_info: dict, tracker_secret: str):
+def add_tracker(
+        tracker_name: str,
+        tracker_type: str,
+        tracker_info: dict,
+        tracker_token: str
+):
     """
     Add Tracker
     :param tracker_name:
     :param tracker_type: jira
     :param tracker_info:
-    :param tracker_secret:
+    :param tracker_token:
     :return: tracker_id
     """
     if tracker_type.lower() == 'jira':
         with JiraSession(
                 tracker_info.get('host'),
                 tracker_info.get('account'),
-                tracker_secret) as jira_session:
+                tracker_token) as jira_session:
             assert tracker_info.get('account') == jira_session.get_user()
     else:
         raise Exception('Only support JIRA')
@@ -37,36 +42,40 @@ def add_tracker(tracker_name: str, tracker_type: str, tracker_info: dict, tracke
         name=tracker_name,
         type=tracker_type,
         info=tracker_info,
-        secret=tracker_secret
+        token=tracker_token
     )
     return tracker.uuid
 
 
 @db_session
-def update_tracker(tracker_id: str, tracker_name: str, tracker_type: str, tracker_info: dict, tracker_secret: str):
+def update_tracker(
+        tracker_id: str,
+        tracker_name: str,
+        tracker_type: str,
+        tracker_info: dict,
+        tracker_token: str
+):
     """
     Update Tracker Info
     :param tracker_id:
     :param tracker_name:
     :param tracker_type: jira
     :param tracker_info:
-    :param tracker_secret:
+    :param tracker_token:
     :return: tracker_id
     """
     item = get(t for t in Tracker if str(t.uuid) == tracker_id)
-    if item:
-        if tracker_type.lower() == 'jira':
-            with JiraSession(
-                    tracker_info.get('host'),
-                    tracker_info.get('account'),
-                    tracker_secret) as jira_session:
-                assert tracker_info.get('account') == jira_session.get_user()
-        else:
-            raise Exception('Only support JIRA')
-        item.name = tracker_name
-        item.type = tracker_type
-        item.info = tracker_info
-        item.secret = tracker_secret
+    if not item:
+        raise Exception('Wrong Tracker ID')
+    if tracker_type.lower() == 'jira':
+        with JiraSession(tracker_info.get('host'), tracker_info.get('account'), tracker_token) as jira_session:
+            assert tracker_info.get('account') == jira_session.get_user()
+    else:
+        raise Exception('ONLY support JIRA')
+    item.name = tracker_name
+    item.type = tracker_type
+    item.info = tracker_info
+    item.token = tracker_token
     return item.uuid
 
 
@@ -75,7 +84,13 @@ def list_tracker(tracker_status=None):
     """
     List All Tracker Info
     :param tracker_status:
-    :return: tracker list
+    :return: [{
+        'id': UUID,
+        'name': string,
+        'type': string,
+        'info': dict,
+        'status': string
+    }]
     """
     trackers = list()
     if tracker_status:
@@ -98,19 +113,24 @@ def get_tracker(tracker_id: str):
     """
     Get Tracker
     :param tracker_id:
-    :return: None or tracker info
+    :return: {
+        'id': UUID,
+        'name': string,
+        'type': string,
+        'info': dict,
+        'status': string
+    }
     """
     item = get(t for t in Tracker if str(t.uuid) == tracker_id)
-    tracker = dict()
-    if item:
-        tracker = {
-            'id': item.uuid,
-            'name': item.name,
-            'type': item.type,
-            'info': item.info,
-            'status': item.status,
-        }
-    return tracker
+    if not item:
+        raise Exception('Wrong Tracker ID')
+    return {
+        'id': item.uuid,
+        'name': item.name,
+        'type': item.type,
+        'info': item.info,
+        'status': item.status,
+    }
 
 
 @db_session
@@ -119,12 +139,21 @@ def set_tracker_status(tracker_id: str, tracker_status: str):
     Set Tracker Status as active, disable or delete
     :param tracker_id:
     :param tracker_status: delete, active, disable
-    :return: tracker_id
+    :return: {
+        'id': UUID,
+        'status': 'string'
+    }
     """
+    if tracker_status.lower() not in ['active', 'disable', 'delete']:
+        raise Exception('Unknown tracker status: %s' % tracker_status)
     item = get(t for t in Tracker if str(t.uuid) == tracker_id)
-    if item:
-        item.status = tracker_status.lower()
-    return item.uuid
+    if not item:
+        raise Exception('Wrong Tracker ID')
+    item.status = tracker_status.lower()
+    return {
+        'id': item.uuid,
+        'status': item.status
+    }
 
 
 @db_session
@@ -132,55 +161,108 @@ def list_tracker_project(tracker_id: str):
     """
     List projects from tracker server
     :param tracker_id:
-    :return:
+    :return: [{
+        'key': string,
+        'value': string
+    }]
     """
-    item = get(t for t in Tracker if str(t.uuid) == tracker_id)
+    tracker = get(t for t in Tracker if str(t.uuid) == tracker_id)
     projects = list()
-    if item.type == 'jira':
+    if tracker.type == 'jira':
         with JiraSession(
-                item.info.get('host'),
-                item.info.get('account'),
-                item.secret) as jira_session:
+            tracker.info.get('host'),
+            tracker.info.get('account'),
+            tracker.token
+        ) as jira_session:
             for p in jira_session.get_projects():
                 projects.append({
-                    'key': p.key,
+                    'key': p.id,
                     'value': p.name
                 })
+    else:
+        raise Exception('ONLY support JIRA')
     return projects
 
 
-@db_session
-def list_tracker_issue_type(tracker_id: str):
-    item = get(t for t in Tracker if str(t.uuid) == tracker_id)
-    issue_types = list()
-    if item.type == 'jira':
+def list_tracker_sprint(tracker_id: str):
+    """
+    List sprints from tracker server
+    :param tracker_id:
+    :return: [{
+        'key': string,
+        'value': string
+    }]
+    """
+    tracker = get(t for t in Tracker if str(t.uuid) == tracker_id)
+    sprints = list()
+    if tracker.type == 'jira':
         with JiraSession(
-                item.info.get('host'),
-                item.info.get('account'),
-                item.secret) as jira_session:
-            for t in jira_session.get_issue_types():
-                issue_types.append({
-                    'key': t.id,
-                    'value': t.name
-                })
-    return issue_types
-
-
-@db_session
-def list_tracker_issue_status(tracker_id: str):
-    item = get(t for t in Tracker if str(t.uuid) == tracker_id)
-    issue_statuses = list()
-    if item.type == 'jira':
-        with JiraSession(
-                item.info.get('host'),
-                item.info.get('account'),
-                item.secret) as jira_session:
-            for s in jira_session.get_issue_statuses():
-                issue_statuses.append({
+                tracker.info.get('host'),
+                tracker.info.get('account'),
+                tracker.token
+        ) as jira_session:
+            for s in jira_session.get_sprints():
+                sprints.append({
                     'key': s.id,
                     'value': s.name
                 })
-    return issue_statuses
+    else:
+        raise Exception('ONLY support JIRA')
+    return sprints
+
+
+def list_tracker_issue_field(tracker_id: str):
+    """
+    List issue fields from tracker server
+    :param tracker_id:
+    :return: [{
+        'key': string,
+        'value': string
+    }]
+    """
+    tracker = get(t for t in Tracker if str(t.uuid) == tracker_id)
+    fields = list()
+    if tracker.type == 'jira':
+        with JiraSession(
+                tracker.info.get('host'),
+                tracker.info.get('account'),
+                tracker.token
+        ) as jira_session:
+            for f in jira_session.get_issue_fields():
+                fields.append({
+                    'key': f['id'],
+                    'value': f['name']
+                })
+    else:
+        raise Exception('ONLY support JIRA')
+    return fields
+
+
+def list_tracker_issue_field_value(tracker_id: str, project_id: str, field_key: str):
+    values = list()
+    items = list()
+    tracker = get(t for t in Tracker if str(t.uuid) == tracker_id)
+    project = get(p for p in Project if str(p.uuid) == project_id)
+    if tracker.type == 'jira':
+        with JiraSession(
+                tracker.info.get('host'),
+                tracker.info.get('account'),
+                tracker.token
+        ) as jira_session:
+            if field_key == 'status':
+                items = jira_session.get_issue_statuses()
+            elif field_key == 'issuetype':
+                items = jira_session.get_issue_types()
+            elif field_key == 'versions':
+                items = jira_session.get_project_versions(pid=project.issue_tracker['project_key'])
+            for item in items:
+                values.append({
+                    'key': item.id,
+                    'value': item.name
+                })
+    else:
+        raise Exception('ONLY support JIRA')
+    return values
 
 
 if __name__ == '__main__':

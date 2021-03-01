33da00cb31dfa5bb66685ef21b7ef54e41744bef
seoktaehyeon <seoktaehyeon@msn.com>
2021-03-01 17:01:36 +0800

    update for svcUser

--- svc/svcClient/services/svcUser.py
+++ svc/svcClient/services/svcUser.py
@@ -11,8 +11,8 @@ def login(body: dict):
     :param body:
     :return:
     """
-    assert body.get('username') == 'admin', u'Username is wrong: %s' % body.get('username')
-    assert body.get('password') == os.getenv('SVC_PASS'), u'Password is wrong'
+    assert body.get('username') == os.getenv('QS_USER'), u'Username is wrong: %s' % body.get('username')
+    assert body.get('password') == os.getenv('QS_PASS'), u'Password is wrong'
     return True
 
 
